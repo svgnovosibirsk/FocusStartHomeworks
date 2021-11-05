6a96ceace16c8f5aa667515961e6807d4dd88f3b
Sergey <a@a.com>
2021-11-05 14:53:17 +0700

    Создал workspace c двумя проектами ДЗ-1 и ДЗ-2. Реализовал потокобезопасный массив в ДЗ-2

---
+++ Homework2/ThreadSaveArray/ThreadSaveArray/ThreadSafeArray.swift
@@ -0,0 +1,66 @@
+//
+//  ThreadSafeArray.swift
+//  ThreadSafeArray
+//
+//  Created by Sergey Golovin on 04.11.2021.
+//
+
+import Foundation
+
+class ThreadSafeArray<Element> {
+    private var internalArray: [Element] = []
+    let isolatedQueue = DispatchQueue(label: "com.isolated.queue", attributes: .concurrent)
+    
+    var isEmpty: Bool {
+        var isFull = false
+        isolatedQueue.sync {
+            isFull = internalArray.isEmpty
+        }
+        return isFull
+    }
+    
+    var  count: Int {
+        var count = 0
+        isolatedQueue.sync {
+            count = internalArray.count
+        }
+        return count
+    }
+    
+    func append(_ item: Element) {
+        isolatedQueue.async(flags: .barrier) {
+            self.internalArray.append(item)
+        }
+    }
+    
+    func remove(at index: Int) {
+        isolatedQueue.async(flags: .barrier) {
+            self.internalArray.remove(at: index)
+        }
+    }
+    
+    subscript(index: Int) -> Element {
+        get {
+            var element: Element!
+            self.isolatedQueue.sync {
+                element = internalArray[index]
+            }
+            return element
+        }
+        
+        set {
+            isolatedQueue.async(flags: .barrier) {
+                self.internalArray[index] = newValue
+            }
+        }
+    }
+
+}
+
+extension ThreadSafeArray where Element: Equatable {
+    func contains(_ element: Element) -> Bool {
+        var hasElement = false
+        isolatedQueue.sync { hasElement = self.internalArray.contains(element) }
+        return hasElement
+    }
+}
---
+++ Homework2/ThreadSaveArray/ThreadSaveArray/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  ThreadSafeArray
+//
+//  Created by Sergey Golovin on 03.11.2021.
+//
+
+import Foundation
+
+var threadSafeArray = ThreadSafeArray<Int>()
+
+let utilityQueue = DispatchQueue.global(qos: .utility)
+let arrayAppendGroup = DispatchGroup()
+
+let firstTask =  {
+    for number in 0...1000 {
+        threadSafeArray.append(number)
+    }
+}
+
+let secondTask =  {
+    for number in 0...1000 {
+        threadSafeArray.append(number)
+    }
+}
+
+utilityQueue.async(group: arrayAppendGroup) {
+    firstTask()
+}
+
+utilityQueue.async(group: arrayAppendGroup) {
+    secondTask()
+}
+
+arrayAppendGroup.notify(queue: DispatchQueue.global()) {
+    
+    print("threadSafeArray.count = \(threadSafeArray.count)")
+}
+
+sleep(2)
